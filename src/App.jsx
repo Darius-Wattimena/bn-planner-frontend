8b1092b47e870f5689b8780b7d34670e5082a92d
Darius Wattimena <dariuswattimena@hotmail.com>
2020-08-11 13:08:59 +0200

    Removed unused packages

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,14 @@ import React, {Suspense} from 'react'
 import 'semantic-ui-css/semantic.min.css'
 import Routes from "./Routes"
 import {SpinnerCircular} from 'spinners-react'
-import ReactNotification from 'react-notifications-component'
-import 'react-notifications-component/dist/theme.css'
 import './Colours.scss'
 import './App.scss'
-import {Sidebar} from "semantic-ui-react";
+import {Sidebar} from "semantic-ui-react"
 
 function App() {
   return (
     <Sidebar.Pushable>
       <div className="App">
-        <ReactNotification/>
         <Suspense fallback={SpinnerCircular}>
           <Routes/>
         </Suspense>
