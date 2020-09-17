c11c76ce740fccd14e2031e990a6d88efb9f4e6c
Darius Wattimena <dariuswattimena@hotmail.com>
2020-09-17 17:32:49 +0200

    Reworked the mobile layout

--- src/components/beatmaps/BeatmapListItem.jsx
+++ src/components/beatmaps/BeatmapListItem.jsx
@@ -39,9 +39,9 @@ const BeatmapListItem = ({users, displayStatus, beatmap, canEdit, setSelectedBea
           </Label>
         } src={"https://assets.ppy.sh/beatmaps/" + beatmap.osuId + "/covers/cover.jpg"}/>
       </Grid.Column>
-      <Grid.Column mobile={6} tablet={6} computer={2}>{beatmap.artist}</Grid.Column>
-      <Grid.Column mobile={6} tablet={6} computer={3}>{beatmap.title}</Grid.Column>
-      <Grid.Column mobile={4} tablet={4} computer={2}>{beatmap.mapper}</Grid.Column>
+      <Grid.Column mobile={8} tablet={8} computer={2}>{beatmap.artist}</Grid.Column>
+      <Grid.Column mobile={8} tablet={8} computer={3}>{beatmap.title}</Grid.Column>
+      <Grid.Column computer={2} only={"computer"}>{beatmap.mapper}</Grid.Column>
       <Grid.Column textAlign={"left"} mobile={8} tablet={8} computer={2}>
         <NominatorDetails users={users} nominated={beatmap.nominatedByBNOne} nominators={beatmap.nominators} nominatorNumber={1} />
       </Grid.Column>
